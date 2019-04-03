3e49e88da9bae83b20343252e02f021a841d46c1
Sion Smith <sion.smith@gmail.com>
2019-04-03 16:32:06 +0100

    - bad bot policy - solution helper policy

---
+++ aws_iam_policy_bad_bot.tf
@@ -0,0 +1,83 @@
+resource "aws_iam_role_policy" "lambda_role_badbot_waf_get_change_token" {
+  count = "${local.BadBotProtectionActivated}"
+  name = "${upper(var.common_tags["Environment"])}-lambda-badbot-waf-get-change-token-role"
+  role = "${aws_iam_role.lambda_badbot_role.id}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "waf-regional:GetChangeToken",
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "lambda_role_badbot_waf_get_and_update_ip_set" {
+  count = "${local.BadBotProtectionActivated}"
+  name = "${upper(var.common_tags["Environment"])}-lambda_role_badbot_waf_get_and_update_ip_set"
+  role = "${aws_iam_role.lambda_badbot_role.id}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "waf-regional:GetIPSet",
+                "waf-regional:UpdateIPSet"
+            ],
+            "Resource": "arn:aws:waf-regional:${data.aws_region}:${data.aws_caller_identity.current.account_id}:ipset/${aws_wafregional_ipset.waf_badbot_set.id}"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "lambda_badbot_role_logs_access" {
+  count = "${local.BadBotProtectionActivated}"
+  name = "${upper(var.common_tags["Environment"])}-lambda_badbot_roleLogsAccess"
+  role = "${aws_iam_role.lambda_badbot_role.id}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+              "logs:CreateLogGroup",
+              "logs:CreateLogStream",
+              "logs:PutLogEvents"
+            ],
+            "Resource": "arn:aws:logs:${data.aws_region}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "lambda_badbot_role_cloudwatch_access" {
+  count = "${local.BadBotProtectionActivated}"
+  name = "${upper(var.common_tags["Environment"])}-lambda_badbot_roleCloudWatchAccess"
+  role = "${aws_iam_role.lambda_badbot_role.id}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "cloudwatch:GetMetricStatistics",
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
---
+++ aws_iam_role_policy_solution_helper.tf
@@ -0,0 +1,37 @@
+resource "aws_iam_role_policy" "solution_helper_roleSolution_Helper_Permissions" {
+  name = "${var.account_name}-solution_helper_roleSolution_Helper_Permissions"
+  role = "${aws_iam_role.solution_helper_role.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:logs:${data.aws_region}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/*"
+    },
+    {
+      "Action": [
+        "lambda:AddPermission",
+        "lambda:CreateFunction",
+        "lambda:DeleteFunction",
+        "lambda:GetFunction",
+        "lambda:UpdateFunctionCode",
+        "lambda:UpdateFunctionConfiguration",
+        "iam:PassRole"
+
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "*"
+      ]
+    }
+  ]
+}
+EOF
+}

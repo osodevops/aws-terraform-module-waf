a9a2a078bd95ce2f0249787003d09e45d253e5b4
Sion Smith <sion.smith@gmail.com>
2019-04-04 07:21:02 +0100

    fixed dependency

--- aws_api_gateway_method_bad_bot.tf
+++ aws_api_gateway_method_bad_bot.tf
@@ -1,6 +1,6 @@
 resource "aws_api_gateway_method" "api_gateway_badbot_method" {
   count         = "${local.is_badbot_protection_activated}"
-  depends_on    = ["aws_lambda_function.lambda_waf_badbot_parser_function", "aws_lambda_permission.lambda_invoke_permission_badbot", "aws_api_gateway_rest_api.api_gateway_badbot"]
+  depends_on    = ["aws_lambda_function.lambda_waf_badbot_parser_function", "aws_lambda_permission.lambda_invoke_badbot_permission", "aws_api_gateway_rest_api.api_gateway_badbot"]
   rest_api_id   = "${aws_api_gateway_rest_api.api_gateway_badbot.id}"
   resource_id   = "${aws_api_gateway_resource.api_gateway_badbot_resource.id}"
   http_method   = "ANY"

1c00e0e86397e5797ee549aabfd0481dab686140
Sion Smith <sion.smith@gmail.com>
2019-04-06 09:42:00 +0100

    added more rules.

--- aws_waf_web_acl.tf
+++ aws_waf_web_acl.tf
@@ -15,6 +15,77 @@ resource "aws_wafregional_web_acl" "waf_web_acl" {
     priority = 201
     rule_id  = "${aws_wafregional_rule.waf_whitelist_rule.id}"
   }
+
+  rule {
+    action {
+      type = "BLOCK"
+    }
+
+    priority = 401
+    rule_id  = "${aws_wafregional_rule.waf_blacklist_rule.id}"
+  }
+
+  /*rule {
+    action {
+      type = "BLOCK"
+    }
+
+    priority = 402
+    rule_id  = "${aws_wafregional_rate_based_rule.waf_http_flood_rule.id}"
+  }*/
+
+  rule {
+    action {
+      type = "BLOCK"
+    }
+
+    priority = 403
+    rule_id  = "${aws_wafregional_rule.waf_scans_probes_rule.id}"
+  }
+  rule {
+    action {
+      type = "BLOCK"
+    }
+
+    priority = 404
+    rule_id  = "${aws_wafregional_rule.waf_ip_reputation_lists_rule1.id}"
+  }
+  rule {
+    action {
+      type = "BLOCK"
+    }
+
+    priority = 405
+    rule_id  = "${aws_wafregional_rule.waf_ip_reputation_lists_rule2.id}"
+  }
+
+  /* disabled together with API Gateway
+  rule {
+    action {
+      type = "BLOCK"
+    }
+
+    priority = 406
+    rule_id  = "${aws_wafregional_rule.WAFBadBotRule.id}"
+  }
+*/
+
+  rule {
+    action {
+      type = "BLOCK"
+    }
+
+    priority = 407
+    rule_id  = "${aws_wafregional_rule.waf_sql_injection_rule.id}"
+  }
+  rule {
+    action {
+      type = "BLOCK"
+    }
+
+    priority = 408
+    rule_id  = "${aws_wafregional_rule.waf_xss_rule.id}"
+  }
 }
 
 resource "aws_wafregional_web_acl_association" "waf_web_acl_association" {

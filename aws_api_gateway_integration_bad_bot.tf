77154abfa392484066dab49ad9a63d55f82e5935
Sion Smith <sion.smith@gmail.com>
2019-04-06 07:31:06 +0100

    fixed root reference.

--- aws_api_gateway_integration_bad_bot.tf
+++ aws_api_gateway_integration_bad_bot.tf
@@ -3,7 +3,7 @@ resource "aws_api_gateway_integration" "api_gateway_badbot_method_root_integrati
   depends_on              = ["aws_api_gateway_rest_api.api_gateway_badbot","aws_api_gateway_method.api_gateway_badbot_method", "aws_lambda_function.lambda_waf_badbot_parser_function"]
   rest_api_id             = "${aws_api_gateway_rest_api.api_gateway_badbot.id}"
   resource_id             = "${aws_api_gateway_rest_api.api_gateway_badbot.root_resource_id}"
-  http_method             = "${aws_api_gateway_method.api_gateway_badbot_method.http_method}"
+  http_method             = "${aws_api_gateway_method.api_gateway_badbot_method_root.http_method}"
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:${data.aws_region.current.name}:lambda:path/2015-03-31/functions/${aws_lambda_function.lambda_waf_badbot_parser_function.arn}/invocations"
